fe0252ca364edd3794f1412d702a77f20527be83
philip <philipatdev@gmail.com>
2023-11-19 17:12:18 +0200

    feat:added shared game controls

--- src/pages/GameOfLife.tsx
+++ src/pages/GameOfLife.tsx
@@ -1,14 +1,27 @@
 import { FC } from 'react';
 import { ReduxGame } from '@/redux/components';
 import { ZustandGame } from '@/zustand/components';
+import { SharedGameControls } from '@/pages/SharedGameControls.tsx';
 
 type GameOfLifeProps = {};
 
 const GameOfLife: FC<GameOfLifeProps> = () => {
   return (
-   <div className="flex justify-between">
-     <ReduxGame />
-     <ZustandGame mirror />
+   <div>
+     <div className="flex justify-between flex-nowrap gap-2 overflow-y-auto">
+       <div className="flex-shrink-0 overflow-y-auto">
+         <h2 className="text-2xl font-bold">Redux Toolkit</h2>
+         <ReduxGame />
+       </div>
+       <div className="flex-shrink-0 overflow-y-auto">
+         <h2 className="text-2xl font-bold">Zustand</h2>
+         <ZustandGame mirror />
+       </div>
+     </div>
+
+    <div className="flex justify-center items-center my-5 border-t-2">
+      <SharedGameControls />
+    </div>
    </div>
   )
 }
